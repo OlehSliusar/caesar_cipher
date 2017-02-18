1d4074c615f8a2e3468ea24728fdd5bc046bae1a
Oleh Sliusar <oleh@olehsliusar.com>
2017-02-18 18:44:41 +0200

    Improve conditionals

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,8 +1,8 @@
 def caesar_cipher(string, shift_factor)
   string.length.times do |i|
-    if (string[i].ord + shift_factor) <= 122
+    if string[i].ord >= 97 && (string[i].ord + shift_factor) <= 122 || string[i].ord >= 65 && (string[i].ord + shift_factor) <= 90
       string[i] = (string[i].ord + shift_factor).chr
-    else
+    elsif string[i].ord >= 97 && string[i].ord <= 122 || string[i].ord >= 65 && string[i].ord <= 90
       string[i] = (string[i].ord + shift_factor - 122 + 96).chr
     end
   end
