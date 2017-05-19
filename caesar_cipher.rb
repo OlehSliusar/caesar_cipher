ea8cacd5853e827bf62da8be7bb981468aa85ba2
Oleh Sliusar <olehsliusar@gmail.com>
2017-05-19 09:53:11 +0300

    Ruby style

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,11 +1,13 @@
 def caesar_cipher(string, shift_size)
-  mapping = Hash.new { |h,k| k }.
-                 merge(make_map('a', shift_size)).
-                 merge(make_map('A', shift_size))
+  mapping = Hash.new { |k| k }
+                .merge(make_map('a', shift_size))
+                .merge(make_map('A', shift_size))
   string.gsub(/./, mapping)
 end
 
 def make_map(first_char, shift_size)
   base = first_char.ord
-  26.times.with_object({}) { |i,h| h[(base+i).chr] = (base+((i+shift_size) % 26)).chr }
+  26.times.with_object({}) do |i, h|
+    h[(base + i).chr] = (base + ((i + shift_size) % 26)).chr
+  end
 end
